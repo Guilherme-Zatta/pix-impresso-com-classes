3e54dc18b0dd4ab7528c8fe5e312cbe5866e690b
Guilherme-Zatta <guilhermezatta@hotmail.com>
2024-05-10 21:03:26 -0300

    Criação das classes relacionadas a um saque.

---
+++ src/classes/Conta.java
@@ -0,0 +1,15 @@
+package classes;
+
+public class Conta {
+    private int idConta;
+    private int numero;
+    private int agencia;
+    private int digitoVerificador;
+    private String nome;
+    private String cpf;
+    private int tipoConta;
+    //  (1 - Conta Correte | 2 - Conta Salário | 3 - Conta Poupança)
+    private String numeroCartao;
+    private String senha;
+    
+}
